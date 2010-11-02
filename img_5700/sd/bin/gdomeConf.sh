f80976bdfa07be1949a538d5a58a182a0bf5c613
phj <phj@fb4c2933-9133-4964-856d-a95e729d1cbc>
2010-11-02 13:35:12 +0000

    git-svn-id: svn://o2droid.phj.hu/droid@18 fb4c2933-9133-4964-856d-a95e729d1cbc

--- img_5700/sd/bin/gdomeConf.sh
+++
@@ -1,7 +0,0 @@
-#
-# Configuration file for using the gdome library
-#
-GDOME_LIBDIR="-L/usr/lib"
-GDOME_LIBS="-lgdome -lglib-2.0   -lxml2 -lz -lm  "
-GDOME_INCLUDEDIR="-I/usr/include -I/usr/include/libgdome -I/usr/include/glib-2.0 -I/usr/lib/glib-2.0/include   -I/usr/include/libxml2  "
-MODULE_VERSION="gdome2-0.8.1"
