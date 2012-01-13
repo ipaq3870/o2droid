9afbcb51c4f53cb0e5674f3cc5f1ccfd5b7ee42e
Arve Hjønnevåg <arve@android.com>
2012-01-13 18:21:25 +0400

    staging: android: lowmemorykiller: Don't wait more than one second for a process...
    staging: android: lowmemorykiller: Don't wait more than one second for a process to die
    
    If a process forked and the child process was killed by the
    lowmemorykiller, the lowmemory killer would be disabled until
    the parent process reaped the child or it died itself.
    
    Signed-off-by: Arve Hjønnevåg <arve@android.com>
    Signed-off-by: Anton Vorontsov <anton.vorontsov@linaro.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kern_2.6.32/drivers/staging/android/lowmemorykiller.c
+++ kern_2.6.32/drivers/staging/android/lowmemorykiller.c
@@ -54,6 +54,7 @@ static size_t lowmem_minfree[6] = {
 static int lowmem_minfree_size = 4;
 
 static struct task_struct *lowmem_deathpending;
+static unsigned long lowmem_deathpending_timeout;
 
 #define lowmem_print(level, x...)			\
 	do {						\
@@ -102,7 +103,8 @@ static int lowmem_shrink(int nr_to_scan, gfp_t gfp_mask)
 	 * this pass.
 	 *
 	 */
-	if (lowmem_deathpending)
+	if (lowmem_deathpending &&
+	    time_before_eq(jiffies, lowmem_deathpending_timeout))
 		return 0;
 
 	if (lowmem_adj_size < array_size)
@@ -185,6 +187,7 @@ static int lowmem_shrink(int nr_to_scan, gfp_t gfp_mask)
 		 */
 #ifdef CONFIG_PROFILING
 		lowmem_deathpending = selected;
+		lowmem_deathpending_timeout = jiffies + HZ;
 		task_handoff_register(&task_nb);
 #endif
 		force_sig(SIGKILL, selected);
