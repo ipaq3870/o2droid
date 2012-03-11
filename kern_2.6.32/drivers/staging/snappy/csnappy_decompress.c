fe640c6ecfcdbaefe374bfa47c64fcca23912a3b
Zeev Tarantov <zeev_tarantov@gmail.com>
2012-03-11 21:39:57 +0200

    Snappy: ARM optimizations. There will be more. In particular ARMv6 has unaligned support, but Google chose to not use it. Why?

--- kern_2.6.32/drivers/staging/snappy/csnappy_decompress.c
+++ kern_2.6.32/drivers/staging/snappy/csnappy_decompress.c
@@ -40,11 +40,6 @@ Zeev Tarantov <zeev.tarantov at gmail.com <http://driverdev.linuxdriverproject.o
 #include "csnappy.h"
 
 
-/* Mapping from i in range [0,4] to a mask to extract the bottom 8*i bits */
-static const uint32_t wordmask[] = {
-	0u, 0xffu, 0xffffu, 0xffffffu, 0xffffffffu
-};
-
 /*
  * Data stored per entry in lookup table:
  *      Range   Bits-used       Description
@@ -288,7 +283,7 @@ csnappy_decompress_noheader(
 		if (opcode & 0x3) {
 			opword = char_table[opcode];
 			extra_bytes = opword >> 11;
-			trailer = get_unaligned_le32(src) & wordmask[extra_bytes];
+			trailer = get_unaligned_le(src, extra_bytes);
 			length = opword & 0xff;
 			src += extra_bytes;
 			trailer += opword & 0x700;
@@ -308,7 +303,7 @@ csnappy_decompress_noheader(
 			}
 			if (unlikely(length > 60)) {
 				extra_bytes = length - 60;
-				length = (get_unaligned_le32(src) & wordmask[extra_bytes]) + 1;
+				length = get_unaligned_le(src, extra_bytes) + 1;
 				src += extra_bytes;
 				available = end_minus5 + 5 - src;
 			}
@@ -354,3 +349,5 @@ EXPORT_SYMBOL(csnappy_decompress);
 MODULE_LICENSE("BSD");
 MODULE_DESCRIPTION("Snappy Decompressor");
 #endif
+
+void __aeabi_unwind_cpp_pr0(void) {};
