547ff5d802536af2ab43dc1e440ccbc253577d22
ipaq3870 <ipaq3870@gmail.com>
2012-04-17 16:49:36 +0800

    ALSA: core - Define llseek fops to avoid BLK from https://github.com/curtisembedded/spica-2.6.38/commit/02f4865fa415a87de28cc8c2e4d798ff46be1cf8

--- kern_2.6.32/sound/core/rawmidi.c
+++ kern_2.6.32/sound/core/rawmidi.c
@@ -377,6 +377,10 @@ static int snd_rawmidi_open(struct inode *inode, struct file *file)
 	if ((file->f_flags & O_APPEND) && !(file->f_flags & O_NONBLOCK)) 
 		return -EINVAL;		/* invalid combination */
 
+	err = nonseekable_open(inode, file);
+	if (err < 0)
+		return err;
+
 	if (maj == snd_major) {
 		rmidi = snd_lookup_minor_data(iminor(inode),
 					      SNDRV_DEVICE_TYPE_RAWMIDI);
@@ -1384,6 +1388,7 @@ static const struct file_operations snd_rawmidi_f_ops =
 	.write =	snd_rawmidi_write,
 	.open =		snd_rawmidi_open,
 	.release =	snd_rawmidi_release,
+	.llseek =	no_llseek,
 	.poll =		snd_rawmidi_poll,
 	.unlocked_ioctl =	snd_rawmidi_ioctl,
 	.compat_ioctl =	snd_rawmidi_ioctl_compat,
