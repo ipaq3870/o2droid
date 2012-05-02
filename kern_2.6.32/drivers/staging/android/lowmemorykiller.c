7e81c4cf42b9b87a2f75318bb0e95e78be4f88cc
ezterry <ezterry3>
2012-05-02 13:34:41 +0800

    staging:lowmemkiller add Fugeswap
    staging:lowmemkiller add Fugeswap
    
    fudgeswap acts as follows:
    
    If set to non zero (defualt is 512k):
            Check for the amount of SWAP_FREE space avalible
            If > 0KB is avalible:
                    if fudgeswap > swapfree:
                            other_file += swapfree
                    else:
                            other_file += fugeswap
    
    In short: we will add in fugeswap as long as its less then the free swap
    
    Setting this to a very large positive number will indicate swap ought
    to be fully used as free (and will slow the system down)
    
    smaller numbers will allow you to put some pressure on SWAP without
    slowing the system down as much.
    
    small negitive numbers will allow the system to be faster at the same
    minfree level.
    
    default is 512 to give a very little bit of pressure to use some swap
    but this can be modified at runtime via:
    /sys/module/lowmemorykiller/parameters/fugeswap
    originally by ezterry, modified by faux123

--- kern_2.6.32/drivers/staging/android/lowmemorykiller.c
+++ kern_2.6.32/drivers/staging/android/lowmemorykiller.c
@@ -38,6 +38,11 @@
 #include <linux/profile.h>
 #include <linux/notifier.h>
 
+#ifdef CONFIG_SWAP
+#include <linux/fs.h>
+#include <linux/swap.h>
+#endif
+
 static uint32_t lowmem_debug_level = 2;
 static int lowmem_adj[6] = {
 	0,
@@ -55,12 +60,16 @@ static int lowmem_minfree[6] = {
 static int lowmem_minfree_size = 4;
 
 static unsigned long lowmem_deathpending_timeout;
+#ifdef CONFIG_SWAP
+static int fudgeswap = 512;
+#endif
 
 #define lowmem_print(level, x...)			\
 	do {						\
 		if (lowmem_debug_level >= (level))	\
 			printk(x);			\
 	} while (0)
+
 
 static int lowmem_shrink(int nr_to_scan, gfp_t gfp_mask)
 {
@@ -76,6 +85,20 @@ static int lowmem_shrink(int nr_to_scan, gfp_t gfp_mask)
 	int other_free = global_page_state(NR_FREE_PAGES);
 	int other_file = global_page_state(NR_FILE_PAGES) -
 						global_page_state(NR_SHMEM);
+
+#ifdef CONFIG_SWAP
+	if(fudgeswap != 0){
+		struct sysinfo si;
+		si_swapinfo(&si);
+
+		if(si.freeswap > 0){
+			if(fudgeswap > si.freeswap)
+				other_file += si.freeswap;
+			else
+				other_file += fudgeswap;
+		}
+	}
+#endif
 
 	if (lowmem_adj_size < array_size)
 		array_size = lowmem_adj_size;
@@ -188,6 +211,10 @@ module_param_array_named(adj, lowmem_adj, int, &lowmem_adj_size,
 module_param_array_named(minfree, lowmem_minfree, uint, &lowmem_minfree_size,
 			 S_IRUGO | S_IWUSR);
 module_param_named(debug_level, lowmem_debug_level, uint, S_IRUGO | S_IWUSR);
+
+#ifdef CONFIG_SWAP
+module_param_named(fudgeswap, fudgeswap, int, S_IRUGO | S_IWUSR);
+#endif
 
 module_init(lowmem_init);
 module_exit(lowmem_exit);
