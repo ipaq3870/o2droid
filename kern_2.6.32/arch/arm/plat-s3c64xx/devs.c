4ef4c2e122a786c2f946adbe48d560569fcb6569
ipaq3870 <ipaq3870@gmail.com>
2012-05-02 16:34:57 +0800

    ramconsole: reduced reserved memory to 1MB and obey .config when build

--- kern_2.6.32/arch/arm/plat-s3c64xx/devs.c
+++ kern_2.6.32/arch/arm/plat-s3c64xx/devs.c
@@ -675,14 +675,14 @@ static struct platform_device pmem_jpeg_device = {
 	.dev		= { .platform_data = &pmem_jpeg_pdata },
 };
 
-#define RAM_CONSOLE_SIZE	(SZ_2M)
+#define RAM_CONSOLE_SIZE	(SZ_1M)
 #define RAM_CONSOLE_START	(RESERVED_PMEM_END_ADDR \
 				- RAM_CONSOLE_SIZE)
 
 static struct resource ram_console_resource[] = {
 	{
 		.start	= RAM_CONSOLE_START,
-		.end	= RAM_CONSOLE_START + SZ_1M - 1,
+		.end	= RAM_CONSOLE_START + RAM_CONSOLE_SIZE - 1,
 		.flags	= IORESOURCE_MEM,
 	}
 };
